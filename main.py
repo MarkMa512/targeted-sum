f046f2d6b480d4cdd7f8a9850ca124423b6e31e1
MarkMa512 <ningzhi04@hotmail.com>
2023-05-09 21:44:10 +0800

    improved documentation

--- main.py
+++ main.py
@@ -10,15 +10,19 @@ logging.basicConfig(format="%(asctime)s %(levelname)s %(filename)s:%(funcName)s(
 logger = logging.getLogger(__name__)
 
 def main()->None:
+    """
+    Read input and target from csv files, validate input and target, find all combinations, filter out redundant combinations, validate result, and export to csv.
+
+    Returns:None
+    """
     logger.info("---reading input file---")
-    input_list = read_csv('input_duplicate_neg.csv')
-    target_list = read_csv('target_duplicate_neg.csv')
+    input_list: List[int] = read_csv('input_duplicate_neg.csv')
+    target_list: List [int] = read_csv('target_duplicate_neg.csv')
 
     logger.info("---validating input and target---")
     if not validate_input_target(input_list, target_list):
         return
     
-    logger.info("---all elements in input are unique---")
     logger.info("---finding combinations---")
     all_combinations: List[List[int]] = sum_combinations(input_list, target_list)
     logger.info("---filtering out redundant combinations---")
